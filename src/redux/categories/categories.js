9d52a048247de53573a70ab16466962ceea58213
29td <nonhlanhlamndebele9@gmail.com>
2022-08-23 11:50:51 +0200

    refractor categories

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,22 +1,11 @@
-const UNDER_CONSTRUCTION = 'UNDER_CONSTRACTION';
-const COMPELET = 'COMPLETE';
+const COMPELET = 'bookstore/categories/COMPLETE';
 
 const categories = [];
 
-export const unCompelet = () => ({
-  type: UNDER_CONSTRUCTION,
-});
-
-export const complete = () => ({
-  type: COMPELET,
-});
-
 const categoriesReducer = (state = categories, action) => {
   switch (action.type) {
-    case UNDER_CONSTRUCTION:
-      return false;
     case COMPELET:
-      return true;
+      return UNDER_CONSTRUCTION;
     default:
       return state;
   }
