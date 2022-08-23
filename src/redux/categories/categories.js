a05e757e27324c2b02204c730f8909a795befc76
29td <nonhlanhlamndebele9@gmail.com>
2022-08-23 11:54:46 +0200

    fix linter errors

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,6 +1,15 @@
+const UNDER_CONSTRUCTION = 'bookstore/categories/UNDER_CONSTRACTION';
 const COMPELET = 'bookstore/categories/COMPLETE';
 
 const categories = [];
+
+export const unCompelet = () => ({
+  type: UNDER_CONSTRUCTION,
+});
+
+export const complete = () => ({
+  type: COMPELET,
+});
 
 const categoriesReducer = (state = categories, action) => {
   switch (action.type) {
